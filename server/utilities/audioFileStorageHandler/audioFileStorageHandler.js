3d5858e78807d67ab439c9fdeb051cf392f47ae1
briankeane <lonesomewhistle@gmail.com>
2015-02-09 22:56:02 -0600

    Added storeCommentary and tests

--- server/utilities/audioFileStorageHandler/audioFileStorageHandler.js
+++ server/utilities/audioFileStorageHandler/audioFileStorageHandler.js
@@ -3,9 +3,21 @@ var s3HighLevel = require('s3').createClient(config.s3Options);
 var AWS = require('aws-sdk');
 AWS.config.region = 'us-west-2';
 var s3 = new AWS.S3();
+var _ = require('lodash');
+var unidecode = require('unidecode');
 
 
 function Handler() {
+  var self = this;
+
+  this.cleanFilename = function (filename) {
+    var cleanedFilename = unidecode(filename)
+                            .replace(/[^a-zA-Z0-9\-\.]/, '')
+                            .replace('_', '-')
+                            .replace(' ', '-');
+    return cleanedFilename;
+  }
+
   this.clearBucket = function (bucket, callback) {
     var listGetter = s3HighLevel.listObjects({ s3Params: { Bucket: bucket } });
 
@@ -50,6 +62,98 @@ function Handler() {
       }
     });
   }
+
+  this.storeSong = function (attrs, callback) {
+    // build key
+    var listGetter = s3HighLevel.listObjects({ s3Params: { Bucket: config["s3Buckets"].SONGS_BUCKET } });
+
+    listGetter.on('data', function (data) {
+      var objects = data.Contents;
+      var nextKeyValue;
+      
+      if (!objects.length) {
+        nextKeyValue = 0;
+      } else {
+        objectKeyNumbers = _.map(objects, function (obj) { return parseInt(obj["Key"].substr(4,10)) });
+
+        // grab the next available key value from the keys strings
+        nextKeyValue = Math.max.apply(Math, objectKeyNumbers);
+      }
+
+      nextKeyValue++;
+
+      var key = '-pl-' + 
+                ('0' * (7 - nextKeyValue.toString())) + 
+                nextKeyValue + '-' +
+                attrs.artist + '-' + 
+                attrs.title  +
+                '.mp3';
+
+      key = self.cleanFilename(key);
+      var metadata = {
+                        pl_artist: (attrs.artist || ''),
+                        pl_title: (attrs.title || ''),
+                        pl_duration: (attrs.duration.toString() || ''),
+                        pl_album: (attrs.album || ''),
+                        pl_echonest_id: (attrs.echonestId || '')
+                      };
+
+      var uploader = s3HighLevel.uploadFile({ localFile: attrs.filepath,
+                                              s3Params: { 
+                                                Bucket: config["s3Buckets"].SONGS_BUCKET,
+                                                Key: key,
+                                                Metadata: metadata
+                                              }
+                                            });
+      uploader.on('end', function (data) {
+        callback(null, key);
+      });
+    });    
+  };
+
+  this.storeCommentary = function (attrs, callback) {
+    // build key
+    var listGetter = s3HighLevel.listObjects({ s3Params: { Bucket: config["s3Buckets"].COMMENTARIES_BUCKET } });
+
+    listGetter.on('data', function (data) {
+      var objects = data.Contents;
+      var nextKeyValue;
+      
+      if (!objects.length) {
+        nextKeyValue = 0;
+      } else {
+        objectKeyNumbers = _.map(objects, function (obj) { return parseInt(obj["Key"].substr(4,10)) });
+
+        // grab the next available key value from the keys strings
+        nextKeyValue = Math.max.apply(Math, objectKeyNumbers);
+      }
+
+      nextKeyValue++;
+
+      var key = '-com' + 
+                ('0' * (7 - nextKeyValue.toString())) + 
+                nextKeyValue + '-' +
+                attrs.stationId +
+                '.mp3';
+
+      key = self.cleanFilename(key);
+      var metadata = {
+                        pl_station_id: attrs.stationId,
+                        pl_duration: (attrs.duration.toString() || ''),
+                      };
+                      console.log('localFile: ' + attrs.songFilepath);
+      var uploader = s3HighLevel.uploadFile({ localFile: attrs.filepath,
+                                              s3Params: { 
+                                                Bucket: config["s3Buckets"].COMMENTARIES_BUCKET,
+                                                Key: key,
+                                                Metadata: metadata
+                                              }
+                                            });
+      uploader.on('end', function (data) {
+        callback(null, key);
+      });
+    });    
+  };
 }
 
 module.exports = new Handler();
--- server/utilities/audioFileStorageHandler/audioFileStorageHandler.spec.js
+++ server/utilities/audioFileStorageHandler/audioFileStorageHandler.spec.js
@@ -2,7 +2,9 @@ var config = require('../../config/environment');
 var s3HighLevel = require('s3').createClient(config.s3Options);
 var AWS = require('aws-sdk');
 AWS.config.region = 'us-west-2';
+var s3 = new AWS.S3();
 var expect = require('chai').expect;
+var Station = require('../../api/station/station.model');
 
 var audioSH = require('./audioFileStorageHandler');
 
@@ -16,7 +18,7 @@ describe('audioFileStorageHandler', function (done) {
     });
   });
 
-  after(function (done) {  
+  afterEach(function (done) {  
     audioSH.clearBucket('playolasongstest', function () {
       audioSH.clearBucket('playolacommentariestest', function () {
         done();
@@ -50,12 +52,47 @@ describe('audioFileStorageHandler', function (done) {
     });
   });
 
-  xit('stores a song', function (done) {
-
+  it('stores a song', function (done) {
+    this.timeout(100000);
+    audioSH.storeSong({ title: 'Look At That Girl',
+                        artist: 'Rachel Loy',
+                        album: 'Broken Machine',
+                        duration: 9999,
+                        echonestId: 'test_echonest_id',
+                        filepath: process.cwd() + '/server/data/testFiles/look.mp3' 
+                      }, function (err, key) {
+      expect(err).to.equal(null);
+      expect(key).to.be.a('string');
+      s3.headObject({ Bucket: 'playolasongstest', Key: key}, function (err, data) {
+        expect(data.ContentLength).to.equal('4961086');
+        expect(data.Metadata.pl_title).to.equal('Look At That Girl');
+        expect(data.Metadata.pl_artist).to.equal('Rachel Loy');
+        expect(data.Metadata.pl_album).to.equal('Broken Machine');
+        expect(data.Metadata.pl_duration).to.equal('9999');
+        expect(data.Metadata.pl_echonest_id).to.equal('test_echonest_id');
+        done();
+      });
+    });
   });
 
-  xit('stores a commentary', function (done) {
-
+  it('stores a commentary', function (done) {
+    this.timeout(100000);
+    station = new Station({ secsOfCommercialsPerHour: 360 });
+    station.save(function (err, savedStation) {
+      audioSH.storeCommentary({ stationId: station.id,
+                                duration: 9999,
+                                filepath: process.cwd() + '/server/data/testFiles/testCommentary.mp3' 
+                              }, function (err, key) {
+        expect(err).to.equal(null);
+        expect(key).to.be.a('string');
+        s3.headObject({ Bucket: 'playolacommentariestest', Key: key }, function (err, data) {
+          expect(data.Metadata.pl_duration).to.equal('9999');
+          expect(data.Metadata.pl_station_id).to.equal(station.id);
+          expect(data.ContentLength).to.equal('497910');
+          done();
+        });
+      });
+    });
   });
 
   xit('updates the metadata on a song', function (done) {
