d43f09832b1e5f66a2b8fc521ab28661186b6045
briankeane <lonesomewhistle@gmail.com>
2015-02-13 15:08:32 -0600

    timezoneFinder and tests added

---
+++ server/utilities/timezoneFinder/timezoneFinder.js
@@ -0,0 +1,65 @@
+var fs = require('fs');
+var csv = require('fast-csv');
+var https = require('https');
+
+
+function TimezoneFinder() {
+  var self = this;
+  var latLongCSVFile = './cityzip.csv';
+
+  
+  this.findByZip = function (zip, callback) {
+    var latitude;
+    var longitude;
+    var state;
+    var city;
+    var foundZip = false;
+
+    var stream = fs.createReadStream(__dirname + '/cityzip.csv');
+
+    stream.pipe(csv())
+    .on("data", function (data) {
+      
+      // if it's the first time it sees the zipcode, send the request
+      if ((data[2] === zip) && !foundZip) {
+        foundZip = true;
+        latitude = data[3];
+        longitude = data[4];
+        state = data[0];
+        city = data[1];
+
+        stream.close();
+
+        var options = {
+          hostname: 'maps.googleapis.com',
+          path: '/maps/api/timezone/json?location=' + latitude + ',' + longitude + '&timestamp=' + Math.floor(Date.now()/1000),
+          method: 'GET'
+        }
+
+        console.log(options.hostname + options.path);
+
+        var req = https.request(options, function (res) {
+          var responseString = '';
+
+          // add data as it comes
+          res.on('data', function (data) {
+            responseString += data;
+          });
+
+          // when finished, parse data and run callback with result
+          res.on('end', function() {
+            var responseObject = JSON.parse(responseString);
+            callback(null, responseObject["timeZoneId"]);
+          });
+        });
+        req.end();
+
+        req.on('error', function (err) {
+          callback(err, null);
+        });
+      }
+    });
+  }
+}
+
+module.exports = new TimezoneFinder();
---
+++ server/utilities/timezoneFinder/timezoneFinder.spec.js
@@ -0,0 +1,25 @@
+var should = require('should');
+var app = require('../../app');
+var request = require('supertest');
+var SpecHelper = require('../helpers/specHelper');
+var TimezoneFinder = require('./timezoneFinder');
+var expect = require('chai').expect;
+
+describe('timezoneFinder', function (done) {
+  this.timeout(10000);
+  it('returns a correct timezone', function (done) {
+    TimezoneFinder.findByZip('78704', function (err, timezone) {
+      expect(timezone).to.equal('America/Chicago')
+      TimezoneFinder.findByZip('90027', function (err, timezone) {
+        expect(timezone).to.equal('America/Los_Angeles');
+        TimezoneFinder.findByZip('80012', function (err, timezone) {
+          expect(timezone).to.equal('America/Denver');
+          TimezoneFinder.findByZip('02215', function (err, timezone) {
+            expect(timezone).to.equal('America/New_York');
+            done();
+          });
+        });
+      });
+    });
+  });
+});
