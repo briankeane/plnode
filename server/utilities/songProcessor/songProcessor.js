8b5b4ab0319db3f25ab5921351a42ab74ca53035
briankeane <lonesomewhistle@gmail.com>
2015-03-04 13:53:09 -0600

    Added tests for m4a file tag reading

--- server/utilities/songProcessor/songProcessor.js
+++ server/utilities/songProcessor/songProcessor.js
@@ -9,11 +9,13 @@ function SongProcessor() {
       if (err) callback(err);
       
       // combine objects
-      tag.duration = audioProperties.length;
+      tag.duration = audioProperties.length * 1000;
       tag.bitrate = audioProperties.bitrate;
       tag.sampleRate = audioProperties.sampleRate;
       tag.channels = audioProperties.channels;
 
+      console.log(audioProperties);
+      console.log(tag);
       callback(null, tag);
 
     });
--- server/utilities/songProcessor/songProcessor.spec.js
+++ server/utilities/songProcessor/songProcessor.spec.js
@@ -6,25 +6,37 @@ describe('songProcessor', function (done) {
   it('gets id3 tags from an mp3 file', function (done) {
     this.timeout(5000);
     SongProcessor.getTags(process.cwd() + '/server/data/testFiles/look.mp3', function (err, tags) {
-      console.log(tags);
-      console.log(audioProperties)
       expect(tags.title).to.equal('Look At That Girl');
       expect(tags.artist).to.equal('Rachel Loy');
-      expect()
+      expect(tags.duration).to.equal(241000);
+      expect(tags.sampleRate).to.equal(44100);
+      expect(tags.channels).to.equal(2);
+      expect(tags.bitrate).to.equal(160);
+      expect(tags.album).to.equal('Broken Machine');
       done();
     });
   });
 
-  xit('gets id4 tags from an encrypted m4a file', function (done) {
-    done();
+  it('gets id4 tags from an encrypted m4a file', function (done) {
+    this.timeout(5000);
+    SongProcessor.getTags(process.cwd() + '/server/data/testFiles/downtown.m4p', function (err, tags) {
+      expect(tags.title).to.equal('Girl Downtown');
+      expect(tags.artist).to.equal('Hayes Carll');
+      expect(tags.album).to.equal('Trouble In Mind');
+      expect(tags.duration).to.equal(207000);
+      done();
+    });
   });
 
-  xit('gets id4 tags from a non-encrypted m4a file', function (done) {
-    done();
-  });
-
-  xit('writes the id3 tags to an mp3 file', function (done) {
-
+  it('gets id4 tags from a non-encrypted m4a file', function (done) {
+    this.timeout(5000);
+    SongProcessor.getTags(process.cwd() + '/server/data/testFiles/lonestar.m4a', function (err, tags) {
+      console.log(tags);
+      expect(tags.title).to.equal('Lone Star Blues');
+      expect(tags.artist).to.equal('Delbert McClinton');
+      expect(tags.album).to.equal('Room to Breathe');
+      done();
+    });
   });
 
 });
