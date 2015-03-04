73d622ce87f386a2f59913a9ab753a17bcc9edd5
briankeane <lonesomewhistle@gmail.com>
2015-03-04 12:47:24 -0600

    Added id3tag reader

--- server/utilities/songProcessor/songProcessor.js
+++ server/utilities/songProcessor/songProcessor.js
@@ -1,1 +1,23 @@
-songProcessor.js
+var Song = require('../../api/song/song.model');
+var taglib = require('taglib');
+
+function SongProcessor() {
+  var self = this;
+
+  this.getTags = function (filepath, callback) {
+    taglib.read(filepath, function (err, tag, audioProperties) {
+      if (err) callback(err);
+      
+      // combine objects
+      tag.duration = audioProperties.length;
+      tag.bitrate = audioProperties.bitrate;
+      tag.sampleRate = audioProperties.sampleRate;
+      tag.channels = audioProperties.channels;
+
+      callback(null, tag);
+
+    });
+  };
+}
+
+module.exports = new SongProcessor();
--- server/utilities/songProcessor/songProcessor.spec.js
+++ server/utilities/songProcessor/songProcessor.spec.js
@@ -1,7 +1,18 @@
+var SongProcessor = require('./songProcessor');
+var expect = require('chai').expect;
+
 describe('songProcessor', function (done) {
   
-  xit('gets id3 tags from an mp3 file', function (done) {
-    done();
+  it('gets id3 tags from an mp3 file', function (done) {
+    this.timeout(5000);
+    SongProcessor.getTags(process.cwd() + '/server/data/testFiles/look.mp3', function (err, tags) {
+      console.log(tags);
+      console.log(audioProperties)
+      expect(tags.title).to.equal('Look At That Girl');
+      expect(tags.artist).to.equal('Rachel Loy');
+      expect()
+      done();
+    });
   });
 
   xit('gets id4 tags from an encrypted m4a file', function (done) {
