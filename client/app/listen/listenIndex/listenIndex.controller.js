486c4f150d8dfa46e6677b1c2a5ca04b646e5096
briankeane <lonesomewhistle@gmail.com>
2015-03-10 14:51:01 -0500

    listenIndex now grabs nowPlaying

--- client/app/listen/listenIndex/listenIndex.controller.js
+++ client/app/listen/listenIndex/listenIndex.controller.js
@@ -2,15 +2,33 @@
 
 angular.module('pl2NodeYoApp')
   .controller('ListenIndexCtrl', function ($scope, Auth, $location, $window, $timeout, AudioPlayer) {
+    
+    $scope.timeouts = [];
+
+
     $timeout(function () {
       Auth.getTwitterFriends(function (err, result) {
         console.log(result.friends);
         $scope.twitterFriends = result.friends;
-      
-      });
+
+        // grab the program for each station
+        for(var i=0;i<$scope.twitterFriends.length;i++) {
+          refreshProgram($scope.twitterFriends[i]);
+        }
+      })
     }, 1000);
 
     $scope.playStation = function (stationId) {
       AudioPlayer.loadStation(stationId);
     }
-  })
+
+    function refreshProgram(friend) {
+      Auth.getProgram({  id: friend._station._id }, function (err, program) {
+        friend.program = program;
+
+        var newTimeout = $timeout(function () {
+          refreshProgram(friend);
+        }, new Date(program.nowPlaying.endTime).getTime() - Date.now());
+      });
+    }
+  });
